bceb4d35562aa7bf5fa160f3c47e1f4696d2d6d4
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-04-30 19:41:43 +0100

    Finished the fifth section
    Footer left

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ const lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smoothWheel: true,
-    wheelMultiplier: 80
+    wheelMultiplier: 70
 })
 
 function raf(time){
