b8d7583a8f9cc86300569ddb20843d19abad328d
Jenrola Jackson <jacksinsthegenius@gmail.com>
2023-08-31 11:31:27 +0100

    reinstall lenis

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ const lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smoothWheel: true,
-    wheelMultiplier: 70
+    wheelMultiplier: 1
 })
 
 function raf(time){
